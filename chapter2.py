8367ea9e20d76f9d64c72f76a1aa0980859f4147
DimitarAtanassov <dimitar789558@gmail.com>
2025-07-24 14:18:46 -0700

    Behind the pipeline examples

--- chapter2.py
+++ chapter2.py
@@ -1,4 +1,5 @@
-from transformers import pipeline, AutoTokenizer, AutoModel
+from transformers import pipeline, AutoTokenizer, AutoModel, AutoModelForSequenceClassification
+import torch
 
 # classifier = pipeline('sentiment-analysis')
 
@@ -74,3 +75,47 @@ High-dimensional vectors usually have 3 dimensions
 Hidden states can be useful on their own, but they are usually inputs to another part of the model, known as the 'head'
 """
 print(f'Model Outputs hidden state: {model_outputs.last_hidden_state.shape}')
+
+# Model Heads: Making sense out of numbers
+"""
+The model heads take the high-dimensional vector of hidden states as input and project them onto a different dimension.
+
+The output of the Transformer model is sent directly to the model head to be processed.
+
+The embeddings layer of the model converts each input ID in the tokenized input into a vector that represents the associated token.
+The subsequent layers manipulate those vectors using the attention mechansim to produce the final representation of the sentences.
+"""
+
+# Example:
+# We will need a model with a sequence classification head (inorder to be able to classify the sentences as positive or negative). So, we will not actually use the AutoModel class, but the AutoModelForSequenceClassification:
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+
+outputs = model(**model_inputs)
+
+# The shape of our outputs, the dimensionality will be much lower:
+# The model head takes as input the high-dimensional vectors we saw before, and ouputs vectors containing two values (one per label)
+print(f'\nSequence Classification Model Outputs:\n {outputs.logits.shape}') # The result we get from our model is of shape 2x2
+
+
+# Postprocessing the output:
+"""
+The values we get as output from our model don't necessarily make sense by themselves.
+  This is because the outputs from our model are NOT probabilities but 'logits'
+
+  Logits: The raw unnormalized scores outputted by the last layer of the model.
+  - To be converted to probabilities, they need to go through a SoftMax layer
+"""
+
+predictions = torch.nn.functional.softmax(outputs.logits, dim=-1) # these are recognizable probability scores
+print('\nPost Processing: Converting logits to probabilities using a SoftMax layer:\n')
+print(f'Logits converted to probabilities aka the model predictions: {predictions}\n')
+
+"""
+To get the labels corresponding to each position, we can inspect the id2label attribute of the model config
+"""
+
+print(f'Labels corresponding to each position:\n{model.config.id2label}\n')
+
